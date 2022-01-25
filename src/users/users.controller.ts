2f68392199c5e056b33e1d21b20b26cb0ba45f76
Valerii Radchenko <valerii.radchenko@freshcodeit.com>
2022-01-25 14:23:47 +0200

    Add new Methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,28 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { create } from 'domain';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Redirect, Header, Req, Res } from '@nestjs/common';
+// import { create } from 'domain';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdateUserDTO } from './dto/update-user.dto';
+// import { Response, Request } from 'express';
 
 @Controller('users')
 export class UsersController {
   @Get()
+  // @Redirect('https://docs.nestjs.com/controllers', 301)
   getAll(): string {
     return 'USERS: 0';
   }
+  //***Express example
+  // getAll(@Req() req: Request, @Res() res: Response): string {
+  //   res.status(201).end('Caka');
+  //   return 'USERS: 0';
+  // }
   @Get(':id')
   getById(@Param('id') id): string {
     return `User by id ${id} is not defined`;
   }
   @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @Header('BCrypt', 'false')
   create(@Body() createUserDTO: CreateUserDTO) {
     return `${createUserDTO.name} id:${createUserDTO.id} created`;
   }
