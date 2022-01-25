1a7907fb9bf706b726a2e651b334a57f4643fd9a
Valerii Radchenko <valerii.radchenko@freshcodeit.com>
2022-01-25 13:48:30 +0200

    Add POST, DELETE, PUT methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { create } from 'domain';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +12,17 @@ export class UsersController {
   @Get(':id')
   getById(@Param('id') id): string {
     return `User by id ${id} is not defined`;
+  }
+  @Post()
+  create(@Body() createUserDTO: CreateUserDTO) {
+    return `${createUserDTO.name} id:${createUserDTO.id} created`;
+  }
+  @Delete(':id')
+  remove(@Param('id') id): string {
+    return `User by id ${id} removed`;
+  }
+  @Put(':id')
+  update(@Body() updateUserDTO: UpdateUserDTO, @Param('id') id) {
+    return `id: ${id} updated success ${updateUserDTO.id == id ? '' : 'new id:' + updateUserDTO.id}`;
   }
 }
