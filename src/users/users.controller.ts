9f9fde422b0b3e3ef82f8adfa419fef78fffa6a8
Valerii Radchenko <valerii.radchenko@freshcodeit.com>
2022-01-25 13:05:19 +0200

    Add main methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get()
-  getAll() {
+  getAll(): string {
     return 'USERS: 0';
+  }
+  @Get(':id')
+  getById(@Param('id') id): string {
+    return `User by id ${id} is not defined`;
   }
 }
